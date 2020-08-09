40787048736250ecb309e29dc051f6ae7dffcb7a
joaomarcos11 <joaomarcos11.tech@gmail.com>
2020-08-09 11:07:49 -0300

    Favoritar e desfavoritar proffy

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, Text, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
@@ -20,9 +21,52 @@ export interface Teacher {
 
 interface TeacherItemProps {
   teacher: Teacher;
+  favorited: boolean;
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+  const [isFavorited, setIsFavorited ] = useState(favorited);
+  
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
+  async function handleToggleFavorite() {
+    // adicionar aos favoritos
+    const favorites = await AsyncStorage.getItem('favorites');
+    // como pode ser vazio, seta logo
+    let favoritesArray = [];
+    // como pode retornar null, verifica se vem
+    if (favorites) {
+      // transforma o favorites em array
+      favoritesArray = JSON.parse(favorites);
+    }
+
+    if (isFavorited) {
+      // remover dos favoritos
+
+      // procurar a posicao do teacher no array de favoritos
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        // faz uma varredura procurando qual a posição da props teacher(.id)
+        // que é o elemento em evidencia atualmente e compara-lo com os itens
+        // dentro do array com os teachers
+        return teacherItem.id === teacher.id;
+      });
+
+      // a partir de qual indice e quantos elementos deve remover 
+      favoritesArray.splice(favoriteIndex, 1);
+      // seta como falso, o elemento esta como nao-favoritado
+      setIsFavorited(false);
+    } else {
+      // add o elemento teacher no array
+      favoritesArray.push(teacher);
+      // seta como true, o elemento esta como favoritado
+      setIsFavorited(true);
+    }
+    // salva no banco de dados, na chave favorites, o array convertido para JSON (texto)
+    await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
@@ -40,16 +84,24 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/hora {'  '}
-          <Text style={styles.priceValue}>{ teacher.cost }</Text>
+          <Text style={styles.priceValue}> R$ { teacher.cost }</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
-          <RectButton style={[styles.favoriteButton, styles.favoritedButton]}>
-            {/* <Image source={heartOutlineIcon} /> */}
-            <Image source={unfavoriteIcon} />
+          <RectButton 
+            onPress={handleToggleFavorite}
+            style={[
+              styles.favoriteButton, 
+              isFavorited ? styles.favoritedButton : {},
+            ]}
+          >
+            { isFavorited 
+              ? <Image source={unfavoriteIcon} /> 
+              : <Image source={heartOutlineIcon} /> 
+            }
           </RectButton>
           
-          <RectButton style={styles.contactButton}>
+          <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
             <Image source={whatsappIcon} />
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
